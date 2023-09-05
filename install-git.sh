033606d1aa918ca8c1e6b364bedf7575d9deab8a
VASUNDHV <vijjagiri.vasundhara123@gmail.com>
2023-09-05 22:46:09 +0530

    git install code

--- install-git.sh
+++ install-git.sh
@@ -2,11 +2,11 @@
 
 # our program goal is install to my sql
 USERID=$(id -u)
-if[$USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
-echo "Error: Please run this script with root access"
-exit 1
+    echo "Error: Please run this script with root access"
+    exit 1
 else
-echo "INFO: You are root user"
+    echo "INFO: You are root user"
 fi
 yum install mysql -y
