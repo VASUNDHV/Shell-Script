ddab3b8820daa07ed1f65327a2744f2f3c12a0e2
VASUNDHV <vijjagiri.vasundhara123@gmail.com>
2023-09-05 23:12:01 +0530

    git installation

--- install-git.sh
+++ install-git.sh
@@ -2,6 +2,19 @@
 
 # our program goal is install to my sql
 USERID=$(id -u)
+# this function should validate the previous command it success or failure
+VALIDATE(){
+    #$1 ---> it will receive the argument1
+    if [ $1 -ne 0 ]
+    then
+        echo "Installation .......... FAILURE"
+        exit 1
+    else
+        echo "Installation .......... SUCCESS"
+    fi
+
+
+}
 if [ $USERID -ne 0 ]
 then
     echo "Error: Please run this script with root access"
@@ -11,20 +24,8 @@ else
 fi
 yum install mysql -y
 
-if[ $? -ne 0 ]
-then
-    echo "Installation of my sql is error"
-    exit 1
-else
-    echo "Installation of mysql is success"
-fi        
+VALIDATE $?        
 
 yum install postfix -y
 
-if[ $? -ne 0 ]
-then
-    echo "Installation of my postfix is error"
-    exit 1
-else
-    echo "Installation of postfix is success"
-fi 
+VALIDATE $?
