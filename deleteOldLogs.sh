de9e7b45624c93f81378a011af4afee556a66b05
VASUNDHV <vijjagiri.vasundhara123@gmail.com>
2023-09-10 18:13:36 +0530

    commit the code

--- deleteOldLogs.sh
+++ deleteOldLogs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 APP_LOGS_DIR=/home/centos/app-logs
-Date=$(date +%F)
+Date=$(date +%F:%H:%M:%s)
 LOGSDIR=/home/centos/ShellScriptLogs
 # /home/centos/ShellScriptLogs/script name-date.log
 SCRIPT_NAME=$0
@@ -8,7 +8,7 @@ LOGFILE=$LOGSDIR/$0-$DATE.log
 
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 
-echo "$FILES_TO_DELETE"
+echo "script started executing at $DATE" &>> $LOGFILE
 
 while read line
 do
