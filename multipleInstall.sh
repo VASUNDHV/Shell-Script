702ad5cbbcc5e842eb5169f6deee7e5b3f09f5d3
VASUNDHV <vijjagiri.vasundhara123@gmail.com>
2023-09-10 16:17:18 +0530

    commit the code

--- multipleInstall.sh
+++ multipleInstall.sh
@@ -2,8 +2,8 @@
 USERID=$(id -u)
 R="\e[31m"
 N="\e[0m"
-if[ $USERID -ne 0 ]
+if [ $USERID -ne 0 ]
 then
-    echo "$R ERROR: Please rn this script with root access $N"
+    echo -e "$R ERROR: Please rn this script with root access $N"
     exit 1
 fi
